13a3975cb55650e6dac0a4f5216fce9cf1546058
Ana Carolina Lôbo <analobo21ime@gmail.com>
2025-03-30 15:39:52 -0300

    Update wsgi.py

--- calculate_distance/wsgi.py
+++ calculate_distance/wsgi.py
@@ -15,6 +15,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calculate_distance.settings')
 
 
 from whitenoise import WhiteNoise
+from pathlib import Path
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 application = get_wsgi_application()
 application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
